f26af1db0afbf3d789780bd46423f8454ede4439
Diego Marcillo Pinzon <dfmarcillop@gmail.com>
2020-03-08 01:01:25 -0500

    Updates

---
+++ precache-manifest.65733e0d454b2b2ee5b5ace302aa32c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d801b1c916d8f755515d0a102b8e9738",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "698f5ecc2e1e5fbe45d2",
+    "url": "/portfolio/static/css/main.0c0cf0a3.chunk.css"
+  },
+  {
+    "revision": "60e7228853e85d3ce392",
+    "url": "/portfolio/static/js/2.eb496acd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.eb496acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698f5ecc2e1e5fbe45d2",
+    "url": "/portfolio/static/js/main.c47bad5c.chunk.js"
+  },
+  {
+    "revision": "e13fb5ea1f5a39d22717",
+    "url": "/portfolio/static/js/runtime-main.8dfcca5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
